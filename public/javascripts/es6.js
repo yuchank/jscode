1bb0a155e30a9eaf2fe814b90f97e082b1b2fa18
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-03-21 00:24:37 +0900

    es6

--- public/javascripts/es6.js
+++ public/javascripts/es6.js
@@ -1,22 +1,66 @@
-$(function () {
+// const
+const pizza = true;
 
-  // const
-  const pizza = true;
-  
-  // let
-  {
-    let topic = 'React';
+// let
+{
+  let topic = 'React';
+}
+
+var div,
+    container = document.getElementById('container');
+
+for (let i = 0; i < 5; i++) {     // var i is a global variable.
+  div = document.createElement('div');
+  div.onclick = function () {
+    alert('This is box #' + i);
   }
+  container.appendChild(div);
+}
 
-  var div,
-      container = document.getElementById('container');
+// template strings
+var a = 'hello';
+var b = 'world';
 
-  for (let i = 0; i < 5; i++) {     // var i is a global variable.
-    div = document.createElement('div');
-    div.onclick = function () {
-      alert('This is box #' + i);
-    }
-    container.appendChild(div);
+console.log(`${a} ${b}`);
+
+// default parameters
+function favoriteActivity(name = 'Shane McConkey', activity = 'skiing') {
+  console.log(`${name} loves ${activity}`);
+}
+
+var defaultPerson = {
+  name: {
+    first: 'Shane',
+    last: 'McConkey'
+  },
+  favActivity: 'skiing'
+};
+
+function logActivity(p = defaultPerson) {
+  console.log(`${p.name.first} loves ${p.favActivity}`);
+}
+
+// arrow functions
+var _lordify = firstName => `${firstName} of Canterbury`;
+var __lordify = (firstName, land) => `${firstName} of ${land}`;
+var ___lordify = (firstName, land) => {
+  if (!firstName) {
+    throw new Error('A firstName is required to lordify');
   }
+  if (!land) {
+    throw new Error('A lord must have a land');
+  }
+  return `${firstName} of ${land}`;
+};
 
-});
+var tahoe = {
+  resorts: ['Kirkwood', 'Squaw', 'Alpine', 'Heavenly', 'Northstar'],
+  print: function (delay = 1000) {
+    setTimeout(() => {
+      console.log(this);
+      console.log(this.resorts.join(', '));    // this is window or tahoe
+    }, delay);
+  }
+};
+
+tahoe.print();
